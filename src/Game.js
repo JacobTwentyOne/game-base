199bfbd1d7a1de8f9fee6f0aa2b625332098128c
jacob.johanssonludv <jacob.johanssonludvigsson@elev.ga.ntig.se>
2023-10-10 10:30:04 +0200

    laggt till pilltangent movement

--- src/Game.js
+++ src/Game.js
@@ -1,19 +1,27 @@
+import InputHandler from './InputHandler.js'
+import Player from './Player.js'
 export default class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
+    this.input = new InputHandler(this)
     this.keys = []
     this.enemies = []
     this.gameOver = false
     this.gravity = 1
     this.debug = false
+
+    this.player = new Player(this)
   }
 
   update(deltaTime) {
     if (!this.gameOver) {
       this.gameTime += deltaTime
     }
+    this.player.update(deltaTime)
   }
 
-  draw(context) {}
+  draw(context) {
+    this.player.draw(context)
+  }
 }
---
+++ src/Player.js
@@ -0,0 +1,40 @@
+export default class Player {
+    constructor(game) {
+        this.game = game;
+        this.width = 32;
+        this.height = 64;
+        this.x = 50;
+        this.y = 100;
+        this.speedX = 0
+        this.speedY = 0
+        this.maxSpeed = 10
+    }
+
+    update(deltaTime) {
+        if (this.game.keys.includes('ArrowLeft')) {
+            this.speedX = -this.maxSpeed
+        } else if (this.game.keys.includes('ArrowRight')) {
+            this.speedX = this.maxSpeed
+        } else {
+            this.speedX = 0
+        }
+
+        this.x += this.speedX
+
+        if (this.game.keys.includes('ArrowUp')) {
+            this.speedY = -this.maxSpeed
+        } else if (this.game.keys.includes('ArrowDown')) {
+            this.speedY = this.maxSpeed
+        } else {
+            this.speedY = 0
+        }
+
+        this.y += this.speedY
+    }
+
+    draw(context) {
+        context.fillStyle = '#f00';
+        context.fillRect(this.x, this.y, this.width, this.height);
+
+    }
+}
